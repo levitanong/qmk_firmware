d0580ae9d537e0f0044f42bf25f4fe0655a09b2f
Levi Tan Ong <levi.ong@gmail.com>
2020-09-13 12:57:50 +0800

    update

--- keyboards/dztech/dz65rgb/keymaps/levitanong/rules.mk
+++ keyboards/dztech/dz65rgb/keymaps/levitanong/rules.mk
@@ -2,3 +2,13 @@ LEADER_ENABLE = yes
 NKRO_ENABLE = no
 CONSOLE_ENABLE = no
 SLEEP_LED_ENABLE = no
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = qmk-dfu
